6bbd29c1616504aa851678e1641eb719ee2bc161
Mamykin <mamykin_87@mail.ru>
2023-07-11 13:36:52 +0300

    task8

--- task8/Program.cs
+++ task8/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все четные числа до N
+
+Console.WriteLine("Введите число");
+string input = Console.ReadLine();
+int N = int.Parse(input);
+Console.WriteLine("Введенное число: {0}", N);
+for (int i = 1; i < N; i++)
+if(i%2==0)
+Console.WriteLine("Четное число: {0}",i);
+
